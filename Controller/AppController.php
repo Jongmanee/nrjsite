39b6f6baf4ddeb9ce050138d51e62fa601f9717c
DESKTOP-SMGU80S\MEGEVAND <raphael.megevand@gmail.com>
2018-03-17 18:01:28 +0100

    login pour qppcontrol

--- Controller/AppController.php
+++ Controller/AppController.php
@@ -43,12 +43,19 @@ class AppController extends Controller
 
         $this->loadComponent('RequestHandler');
         $this->loadComponent('Flash');
-
-        /*
-         * Enable the following components for recommended CakePHP security settings.
-         * see https://book.cakephp.org/3.0/en/controllers/components/security.html
-         */
-        //$this->loadComponent('Security');
-        //$this->loadComponent('Csrf');
+        $this->loadComponent('Auth', [
+            'authenticate' => [
+                'Form' => [
+                    'fields' =>[
+                        'username' => 'email',
+                        'password' => 'password'
+                    ]
+                ]
+            ],
+            'loginAction' => [
+                'controller' => 'Users',
+                'action' => 'login'
+            ]
+        ]);   
     }
 }
